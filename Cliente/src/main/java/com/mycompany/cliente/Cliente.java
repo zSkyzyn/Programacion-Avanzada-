3daf429864a35d4b4ef49fef183d4581df61cc2e
Lopez Luciano <lopezluciano.nm@gmail.com>
2026-04-15 12:16:05 -0300

    Add files via upload

---
+++ Cliente/src/main/java/com/mycompany/cliente/Cliente.java
@@ -0,0 +1,43 @@
+
+
+package com.mycompany.cliente;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Cliente {
+    public static void main(String[] args) {
+        String direccion = "127.0.0.1";
+        int puerto = 5000;
+
+        try (Socket miSocket = new Socket(direccion, puerto);
+             BufferedReader entradaServidor = new BufferedReader(new InputStreamReader(miSocket.getInputStream()));
+             PrintWriter salidaServidor = new PrintWriter(miSocket.getOutputStream(), true);
+             Scanner inputTeclado = new Scanner(System.in)) {
+
+            System.out.println("=== TERMINAL DE ENLACE ESTABLECIDA ===");
+            // Leemos el mensaje de bienvenida del servidor
+            System.out.println("STATUS: " + entradaServidor.readLine());
+
+            while (true) {
+                System.out.print("Escribir comando >> ");
+                String comando = inputTeclado.nextLine();
+                
+                // Enviamos al servidor
+                salidaServidor.println(comando);
+
+                // Esperamos y mostramos respuesta
+                String respuesta = entradaServidor.readLine();
+                System.out.println("SERVIDOR responde >> " + respuesta);
+
+                if (comando.equalsIgnoreCase("FIN")) {
+                    System.out.println("Cerrando aplicación cliente...");
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error: No se pudo establecer el enlace con el servidor.");
+        }
+    }
+}
